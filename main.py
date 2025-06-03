e446b943282563f7c7c1e67b568f532f9c0a9681
Tarkus43 <jeka.spets333@gmail.com>
2025-06-03 19:00:31 +0200

    started learning bot making

---
+++ main.py
@@ -0,0 +1,21 @@
+import telebot
+
+
+
+key = '6886283736:AAEgEXByx3CM39IAOFe6mwNtqitWmypftMo'
+bot = telebot.TeleBot(key)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, f'Привет!, {message.from_user.user_name}')
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Привет, я бот <b>Жабьего императора Жмерлина</b>! Я создан с целью обучения моего господина', parse_mode='html')
+
+
+
+bot.infinity_polling()
+
